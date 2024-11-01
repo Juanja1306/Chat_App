5ed816e01cb0ef99bd83eb4a472be7564a97aeef
Roberto Romero <robertoart76x@gmail.com>
2024-11-01 11:18:09 -0500

    actualizacion del back end

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors'); // Permitir solicitudes de CORS para conectar con el Frontend
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('¡Hola, mundo desde el Backend!');
+// Habilitar CORS para permitir solicitudes desde el Frontend en Angular
+app.use(cors());
+app.use(express.json()); // Para poder recibir datos en formato JSON
+
+//app.use(express.static(path.join(__dirname, 'public')));
+
+// API para obtener un mensaje de prueba
+app.get('/api/status', (req, res) => {
+    res.json({ status: 'API funcionando correctamente' });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// Configuración de Socket.IO para manejar conexiones y mensajes en tiempo real
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado');
+
+    // Escuchar los mensajes enviados por el cliente
+    socket.on('chatMessage', (data) => {
+        io.emit('chatMessage', data); // Enviar el mensaje a todos los clientes conectados
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Un usuario se ha desconectado');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor API ejecutándose en http://localhost:${PORT}`);
 });
